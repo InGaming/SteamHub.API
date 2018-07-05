7a0621f30a78572011841e2c5b93093dab4c1afe
AGD <agdholo@gmail.com>
2018-07-05 22:46:52 +0800

    add redis

--- start/app.js
+++ start/app.js
@@ -20,7 +20,8 @@ const providers = [
   '@adonisjs/session/providers/SessionProvider',
   '@adonisjs/auth/providers/AuthProvider',
   '@adonisjs/websocket/providers/WsProvider',
-  '@adonisjs/drive/providers/DriveProvider'
+  '@adonisjs/drive/providers/DriveProvider',
+  '@adonisjs/redis/providers/RedisProvider'
 ]
 
 /*
