2cb5feffc75f9aa23dcd016cfe1940c394b3983d
AGD <agdholo@gmail.com>
2018-07-07 18:04:30 +0800

    add model relationships

--- app/Models/Api/V1/Steam/Game/List.js
+++ app/Models/Api/V1/Steam/Game/List.js
@@ -6,6 +6,10 @@ class List extends Model {
   static get table () {
     return 'steam_games'
   }
+
+  prices () {
+    return this.hasMany('App/Models/Api/V1/Steam/Game/Price', 'appid', 'appid')
+  }
 }
 
 module.exports = List
--- app/Models/Api/V1/Steam/Game/Price.js
+++ app/Models/Api/V1/Steam/Game/Price.js
@@ -6,6 +6,10 @@ class Price extends Model {
   static get table () {
     return 'steam_game_prices'
   }
+
+  lists () {
+    return this.belongsTo('App/Models/Api/V1/Steam/Game/List', 'appid', 'appid')
+  }
 }
 
 module.exports = Price
