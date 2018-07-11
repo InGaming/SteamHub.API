2ad1c5631346849deb32d2dccbd84edd8b5847ec
AGD <agdholo@gmail.com>
2018-07-11 15:10:51 +0800

    add gameinfo query

--- app/Controllers/Http/Api/V1/Steam/AppInfoController.js
+++ app/Controllers/Http/Api/V1/Steam/AppInfoController.js
@@ -5,12 +5,11 @@ const Redis = use('Redis')
 class AppInfoController {
   async show ({ params }) {
     const AppID = params.id
-    return await GetGameApps.query().with('AppsInfo').where('AppID', AppID).fetch()
     const Cached = await Redis.get('GameAppID=' + AppID)
     if (Cached) {
       return JSON.parse(Cached)
     }
-    const GameAppID = await GetGameApps.query().with('AppsTypes').where('AppID', AppID).fetch()
+    const GameAppID = await GetGameApps.query().with('AppsInfo').with('AppsTypes').where('AppID', AppID).fetch()
     await Redis.set('GameAppID=' + AppID, JSON.stringify(GameAppID.toJSON()), 'ex', 600)
     return GameAppID.toJSON()
   }
