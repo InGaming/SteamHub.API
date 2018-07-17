f39d9304c9da9cdca9b0fcdb81db3efdc3285c4d
AGD <agdholo@gmail.com>
2018-07-17 18:14:23 +0800

    add LastUpdated

--- app/Models/Api/V1/Steam/Game/Price.js
+++ app/Models/Api/V1/Steam/Game/Price.js
@@ -7,6 +7,10 @@ class Price extends Model {
     return 'AppsPrices'
   }
 
+  static get dates () {
+    return super.dates.concat(['LastUpdated'])
+  }
+
   // 反向关联 Apps 表
   Apps () {
     return this.belongsTo('App/Models/Api/V1/Steam/App', 'AppID', 'AppID')
