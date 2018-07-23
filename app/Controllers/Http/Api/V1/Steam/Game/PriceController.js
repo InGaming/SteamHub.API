d4a68129b0e20d28244c563d25559aaf3b8480c8
AGD <agdholo@gmail.com>
2018-07-23 19:26:40 +0800

    Adjust the price into the table

--- app/Controllers/Http/Api/V1/Steam/Game/PriceController.js
+++ app/Controllers/Http/Api/V1/Steam/Game/PriceController.js
@@ -57,28 +57,102 @@ class PriceController {
     const appList = await GetGameApps.query().select('appid').fetch()
     const arrayAppList = appList.toJSON()
 
+    let country = {
+      '中国': {
+        cc: 'cn',
+        country: 'China',
+        displayCountryName: '中国',
+        displayCountrySymbol: '元'
+      },
+      '美国': {
+        cc: 'us',
+        country: 'United States',
+        displayCountryName: '美国',
+        displayCountrySymbol: '美元'
+      },
+      '俄罗斯': {
+        cc: 'ru',
+        country: 'Russia',
+        displayCountryName: '俄罗斯',
+        displayCountrySymbol: '卢布'
+      },
+      '英国': {
+        cc: 'uk',
+        country: 'United Kingdom',
+        displayCountryName: '英国',
+        displayCountrySymbol: '英镑'
+      }
+    }
     const appidList = _.map(arrayAppList, 'appid')
     const chunkAppidList = _.chunk(appidList, 20)
     for (let i = 0; i < chunkAppidList.length; i++) {
       setTimeout(function timer() {
-        requested('https://store.steampowered.com/api/appdetails?appids=' + chunkAppidList[i] + '&cc=cn&filters=price_overview', function (error, response, body) {
-          let parseBody = JSON.parse(body)
-          for (let key in parseBody) {
-            if (!_.isEmpty(parseBody[key]['data'])) {
-              if (!_.isEmpty(parseBody[key]['data']['price_overview'])) {
-                Price.create({
+        for (let item in country) {
+          setTimeout(function timer() {
+            requested('https://store.steampowered.com/api/appdetails?appids=' + chunkAppidList[i] + '&cc=' + country[item]['cc'] + '&filters=price_overview', async function (error, response, body) {
+              let parseBody = JSON.parse(body)
+              let Country = country[item]['country']
+              let DisplayCountryName = country[item]['displayCountryName']
+              let DisplayCountrySymbol = country[item]['displayCountrySymbol']
+              for (let key in parseBody) {
+                let oldPriceFirst = await Price.query().where('AppID', key).where('Country', country[item]['country']).orderBy('LastUpdated', 'desc').first()
+                let oldPrice = await Price.query().where('AppID', key).where('Country', country[item]['country']).orderBy('LastUpdated', 'desc')
+                let insertPriceNull = {
                   AppID: key,
-                  Country: parseBody[key]['data']['price_overview']['currency'],
-                  PriceInitial: parseBody[key]['data']['price_overview']['initial'],
-                  PriceFinal: parseBody[key]['data']['price_overview']['final'],
-                  PriceDiscount: parseBody[key]['data']['price_overview']['discount_percent']
-                })
+                  Country: Country,
+                  DisplayCountryName: DisplayCountryName,
+                  DisplayCountrySymbol: DisplayCountrySymbol,
+                  PriceInitial: null,
+                  PriceFinal: null,
+                  PriceDiscount: null
+                }
+    
+                if (!_.isEmpty(parseBody[key]['data'])) {
+                  if (!_.isEmpty(parseBody[key]['data']['price_overview'])) {
+                    let insertPrice = {
+                      AppID: key,
+                      Country: Country,
+                      DisplayCountryName: DisplayCountryName,
+                      DisplayCountrySymbol: DisplayCountrySymbol,
+                      PriceInitial: parseBody[key]['data']['price_overview']['initial'],
+                      PriceFinal: parseBody[key]['data']['price_overview']['final'],
+                      PriceDiscount: parseBody[key]['data']['price_overview']['discount_percent']
+                    }
+    
+                    if (oldPriceFirst === null || oldPriceFirst === undefined ) {
+                      Price.create(insertPrice)
+                    } else if (oldPriceFirst.PriceFinal !== parseBody[key]['data']['price_overview']['final']) {
+                      Price.create(insertPrice)
+                    } else if (oldPriceFirst.PriceFinal === parseBody[key]['data']['price_overview']['final']) {
+                      if (oldPrice.length === 1) {
+                        Price.create(insertPrice)
+                      } else {
+                        await Price.query().where('AppID', key).where('Country', country[item]['country']).update(insertPrice)
+                      }
+                    }
+    
+                  } else if (_.isEmpty(parseBody[key]['data']['price_overview'])) {
+    
+                    if (oldPriceFirst === null || oldPrice.length === 1) {
+                      Price.create(insertPriceNull)
+                    } else {
+                      await Price.query().where('AppID', key).where('Country', country[item]['country']).update(insertPriceNull)
+                    }
+    
+                  }
+                } else if (_.isEmpty(parseBody[key]['data'])) {
+                  if (oldPriceFirst === null || oldPrice.length === 1) {
+                    Price.create(insertPriceNull)
+                  } else {
+                    await Price.query().where('AppID', key).where('Country', country[item]['country']).update(insertPriceNull)
+                  }
+                }
               }
-            }
-          }
-
-        })
-      }, i * 3000);
+    
+            })
+          }, i * 10000)
+        }
+      }, i * 10000)
     }
     return
   }
