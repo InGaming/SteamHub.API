7076d030b2cf1107ada4efce0507293406ca0861
agdholo <agdholo@gmail.com>
2018-10-21 12:33:52 +0800

    fix: filter shouyou

--- app/Controllers/Http/Api/V1/News/GamerSkyController.js
+++ app/Controllers/Http/Api/V1/News/GamerSkyController.js
@@ -59,9 +59,10 @@ class GamerSkyController {
           })
           const arrayData = _.merge(title, description, type, image, link, site)
           arrayData.forEach(async element => {
-            if (element.title) {
+            console.log(element.type)
+            if (element.title && element.type !== '新游') {
               let newsData =  await News.query().where('title', element.title)
-              if (newsData.length === 0 && element.type !== '手游') {
+              if (newsData.length === 0) {
                 News.create({
                   Title: element.title,
                   Description: element.description,
@@ -69,6 +70,70 @@ class GamerSkyController {
                   Type: '新闻',
                   Site: element.site,
                   Image: element.image
+                })
+              }
+            }
+          })
+        }
+      })
+    }
+
+    function evalData () {
+      requested.get('https://www.gamersky.com/review/pc/',{
+        headers: {
+          'User-Agent': 'Baiduspider'
+        }
+      }, async function (error, body) {
+        if (true) {
+          let data = body.body
+          const $ = cheerio.load(data)
+          let title = []
+          let site = []
+          let image = []
+          let link = []
+          let description = []
+          let star = []
+          $('.pictxt .tit a').each((idx, element) => {
+            title.push({
+              title: $(element).text()
+            })
+            site.push({
+              site: 'gamersky'
+            })
+          })
+          $('.pictxt .con .txt').each((idx, element) => {
+            description.push({
+              description: $(element).text()
+            })
+          })
+          $('.pictxt .img a img').each((idx, element) => {
+            image.push({
+              image: $(element).attr('src')
+            })
+          })
+          $('.pictxt .tit a').each((idx, element) => {
+            link.push({
+              link: $(element).attr('href')
+            })
+          })
+          $('ul.pictxt li .pc .num').each((idx, element) => {
+            star.push({
+              star: $(element).text()
+            })
+          })
+          const arrayData = _.merge(title, description, type, image, link, site)
+          arrayData.forEach(async element => {
+            if (element.title) {
+              let newsData =  await News.query().where('title', element.title)
+              if (newsData.length === 0) {
+                News.create({
+                  Title: element.title,
+                  Description: element.description,
+                  Link: element.link,
+                  Type: '评测',
+                  Site: element.site,
+                  Image: element.image,
+                  Star: element.star
                 })
               }
             }
